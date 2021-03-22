c655b6ed4e2b8970111cf431cf059ceb190bde45
ImenKhessib <imen.kheessib@gmail.com>
2021-03-22 09:50:03 +0100

    update2(22/03/2021)

--- create/views.py
+++ create/views.py
@@ -13,18 +13,6 @@ def creation_dem(request):
     return render(request, 'create_demand.html', {'OFId': 1003211})
 
 
-def creation_dem1(request):
-    if request.method == "POST":
-        qty = request.POST["quantity"]
-        ref = request.POST["ref"]
-        desig = request.POST["desig"]
-
-        return render(request, 'create_demand.html', {'OFId': 1003211, 'qty': qty, 'ref': ref, 'desig': desig})
-
-    else:
-        return render(request, 'create_demand.html', {'OFId': 1003211})
-
-
 def edit_dem(request):
     return render(request, 'edit_demand.html', {'OFId': 1003211})
 
